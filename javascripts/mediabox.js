cc6f3f934527bb8dc2a99b61662d63ab9016c279
AJ ONeal <coolaj86@gmail.com>
2011-05-14 18:15:04 +0000

    thanks to mom for motivating some immediate changes

--- javascripts/mediabox.js
+++ javascripts/mediabox.js
@@ -119,7 +119,8 @@ var tags
       ev.preventDefault();
 
       var MD5SUM = 0
-        , TITLE = 1; 
+        , TITLE = 1
+        , ITEM = 2; 
 
       var query = $('#query').val()
         , pattern = new RegExp(query, 'i')
@@ -132,6 +133,16 @@ var tags
       $('#results').html('');
 
       results.sort(function (a, b) {
+        var atag = a[ITEM]
+          , aartist = atag.artist || ''
+          , aalbum = atag.album || ''
+          , atitle = atag.title || ''
+          , btag = b[ITEM]
+          , bartist = btag.artist || ''
+          , balbum = btag.album || ''
+          , btitle = btag.title || '';
+
+        if (aartist > bartist)
         if (basename(a[TITLE]) > basename(b[TITLE])) {
           return 1;
         }
@@ -145,33 +156,86 @@ var tags
         var md5sum = item[MD5SUM]
           , resource = pathFromMd5sum(md5sum) + extname(item[TITLE]);
 
+        var whattoshow = (item[ITEM].track || '').substr(0,2) + ' - ' + (item[ITEM].title || '') + ' - ' + (item[ITEM].artist || '');
+
+        if (whattoshow.length < 8) {
+          whattoshow = basename(item[TITLE]);
+        }
+
         html.push("" + 
-          "<a class='audioitem' href='" + resource + "'>Add to playlist</a>   " + 
-          basename(item[TITLE]) + ' [' + extname(item[TITLE]).substr(1) + '] ' + 
-          "<a class='encache' href='" + resource + "'>[Cache Locally]</a>   " + 
-          "\n");
+          "<div class='resultitem' >" +
+            "<a class='addaudioitem' href='" + resource + "'>[+]</a>   " + 
+            "<a class='playaudioitem' href='" + resource + "'>[play now]</a>   " + 
+            whattoshow +
+            ' [' + extname(item[TITLE]).substr(1) + '] ' +
+            "<a class='encache' href='" + resource + "'>[Cache Locally]</a>   " + 
+          "</div>"
+        );
       });
 
       $('#results').html(html.join('\n'));
     }
 
+
+
+// playlist events
     var playing = false;
+
+    function onSongEnded(ev) {
+      console.log('mediaEvent', ev);
+      $($('.playlistitem')[0]).remove();
+      playing = false;
+      // TODO nextTick
+      playNext();
+    }
+
+// TODO create search results from global.tags
+// TODO select random song from global.tags when playlist is empty
+// TODO shuffle by default
+// TODO add next just seconds before the first ends
     function playNext() {
       if (playing) {
         return;
       }
+      if (!$('audio') || !$('audio')[0]) {
+        // TODO get a better autoshuffle
+        //return $($('a.addaudioitem')[next]).click();
+        if ($('.resultitem') && $('.resultitem').length > 0) {
+          var next = Math.floor(Math.random() * $('.resultitem').length);
+          console.log('auto selecting from list at random', $($('a.addaudioitem')[next]));
+          setTimeout(function () {
+            $($('.resultitem a.addaudioitem')[next]).click();
+            $($('.resultitem')[next]).remove();
+          }, 10);
+        }
+        return;
+      }
       $('audio')[0].play();
-      $('audio')[0].addEventListener('ended', function (ev) {
-        console.log('mediaEvent', ev);
-        $($('audio')[0]).remove();
-        playing = false;
-        // TODO nextTick
-        playNext();
-      }, false);
+      $('audio')[0].addEventListener('ended', onSongEnded, false);
+      $('audio')[0].addEventListener('error', onSongEnded, false);
       playing = true;
     }
 
-    function handleAddToPlayList(ev) {
+    function onPlayNow(ev) {
+      ev.preventDefault();
+      var resource = $(ev.target).attr('href');
+      if (!$('audio') || !$('audio')[0]) {
+        return onAddToPlaylist(ev);
+      }
+      //$('audio')[0].pause();
+      $($('.playlistitem')[0]).remove();
+      $("#playlist").prepend("" +
+        // if preload=none, can't auto-play
+        "<div class='playlistitem'>" +
+          "<audio preload='metadata' controls><source src='" + resource + "' /></audio> " + 
+        "</div>"
+      );
+      $('audio')[0].addEventListener('ended', onSongEnded, false);
+      $('audio')[0].addEventListener('error', onSongEnded, false);
+      $('audio')[0].play();
+    }
+
+    function onAddToPlaylist(ev) {
       ev.preventDefault();
       console.log(ev.target);
       console.log(ev.target.href);
@@ -179,12 +243,15 @@ var tags
       console.log('href', resource);
       $("#playlist").append("" +
         // if preload=none, can't auto-play
+      "<div class='playlistitem'>" +
         "<audio preload='metadata' controls><source src='" + resource + "' /></audio> " + 
-      "\n<br/>");
+      "</div>");
       playNext();
     }
 
     $('body').delegate('form#search', 'submit', handleSearch);
-    $('body').delegate('a.audioitem', 'click', handleAddToPlayList);
+    $('body').delegate('form#search', 'webkitspeechchange', handleSearch);
+    $('body').delegate('a.addaudioitem', 'click', onAddToPlaylist);
+    $('body').delegate('a.playaudioitem', 'click', onPlayNow);
   });
 }());
