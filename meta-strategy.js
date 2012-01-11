0cf13f89b856bdecfc4307b8fc31c105496cd917
AJ ONeal <coolaj86@gmail.com>
2012-01-11 13:32:00 -0500

    working stub for meta-data saving

--- meta-strategy.js
+++ meta-strategy.js
@@ -1,4 +1,16 @@
-      // TODO meta-saver is a different strategy
-      function saveMeta() {
-      }
+(function () {
+  "use strict";
 
+  function create (options) {
+    options = options || {};
+  
+    function saveMeta(cb, fileStats) {
+      // mtime, size
+      cb();
+    }
+
+    return saveMeta;
+  }
+
+  module.exports.create = create;
+}());
--- test-copy-strategy.js
+++ test-copy-strategy.js
@@ -10,10 +10,12 @@
     an existing (md5 match) file is added
   */
 
-  var Copy = require('./copy-strategy')
-    , fs = require('fs')
+  var fs = require('fs')
     , path = require('path')
+    , Copy = require('./copy-strategy')
     , copy = Copy.create()
+    , Meta = require('./meta-strategy')
+    , metaStore = Meta.create()
     ;
 
   function gotErDone(e, stat) {
@@ -21,6 +23,16 @@
     console.error(e);
     console.log('All Done');
     console.log(stat);
+  }
+
+  function saveMeta(e, fileStats) {
+    if (e) {
+      gotErDone(e, fileStats);
+    }
+
+    metaStore(function (e) {
+      gotErDone(e, fileStats);
+    });
   }
 
   function getStats(e, fileStats) {
@@ -35,7 +47,7 @@
     fileStats.filepath = fullpath.substr(0, fullpath.lastIndexOf('/'));
     fileStats.name = fullpath.substr(fullpath.lastIndexOf('/') + 1);
     //copyAndChecksum();
-    copy(gotErDone, fullpath, fileStats);
+    copy(saveMeta, fullpath, fileStats);
   }
 
   var fullpath = './testroot/absolute/real';
