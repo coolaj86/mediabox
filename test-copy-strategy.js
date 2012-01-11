f34ace8007b966aa30765a270c2cc01812b24798
AJ ONeal <coolaj86@gmail.com>
2012-01-11 16:03:01 -0500

    this test is starting to look like the driver...

--- test-copy-strategy.js
+++ test-copy-strategy.js
@@ -13,50 +13,62 @@
   var fs = require('fs')
     , path = require('path')
     , Copy = require('./copy-strategy')
-    , copy = Copy.create()
     , Meta = require('./meta-strategy')
-    , metaStore = Meta.create()
-    , givenpath = './testroot/absolute/real';
     ;
 
-  function gotErDone(e, stat) {
-    console.error('[error]');
-    console.error(e);
-    console.log('All Done');
-    console.log(stat);
-  }
+  function create(options) {
+    var copy = Copy.create()
+      , metaStore = Meta.create()
+      , givenpath
+      ;
 
-  function saveMeta(e, fileStats) {
-    if (e) {
-      gotErDone(e, fileStats);
+    function gotErDone(e, stat) {
+      console.error('[error]');
+      console.error(e);
+      console.log('All Done');
+      console.log(stat);
     }
 
-    metaStore(function (e) {
-      gotErDone(e, fileStats);
-    }, givenpath, fileStats);
-  }
+    function saveMeta(e, fileStats) {
+      if (e) {
+        gotErDone(e, fileStats);
+      }
 
-  function getStats(e, fileStats) {
-    if (e) {
-      console.error('[ERROR] cannot stat ' + givenpath, e.message);
-      console.error(e.stack);
-      cb(e);
-      return;
+      metaStore(function (e) {
+        gotErDone(e, fileStats);
+      }, givenpath, fileStats);
     }
-    
-    // is this right?
-    //fileStats.pathname = givenpath;
-    fileStats.filepath = givenpath;
-    fileStats.path = givenpath.substr(0, givenpath.lastIndexOf('/'));
-    fileStats.name = givenpath.substr(givenpath.lastIndexOf('/') + 1);
-    //copyAndChecksum();
 
-    fs.realpath(path.resolve(process.cwd(), givenpath), function (err, realpath) {
-      fileStats.realpath = realpath.substr(0, realpath.lastIndexOf('/'));
-      fileStats.realname = realpath.substr(realpath.lastIndexOf('/') + 1);
-      copy(saveMeta, givenpath, fileStats);
-    });
+    function getStats(e, fileStats) {
+      if (e) {
+        console.error('[ERROR] cannot stat ' + givenpath, e.message);
+        console.error(e.stack);
+        cb(e);
+        return;
+      }
+      
+      // is this right?
+      //fileStats.pathname = givenpath;
+      fileStats.filepath = givenpath;
+      fileStats.path = givenpath.substr(0, givenpath.lastIndexOf('/'));
+      fileStats.name = givenpath.substr(givenpath.lastIndexOf('/') + 1);
+      //copyAndChecksum();
+
+      fs.realpath(path.resolve(process.cwd(), givenpath), function (err, realpath) {
+        fileStats.realpath = realpath.substr(0, realpath.lastIndexOf('/'));
+        fileStats.realname = realpath.substr(realpath.lastIndexOf('/') + 1);
+        copy(saveMeta, givenpath, fileStats);
+      });
+    }
+
+    function handleFile(_givenpath) {
+      givenpath = _givenpath;
+      fs.lstat(givenpath, getStats);
+    }
+
+    return handleFile;
   }
 
-  fs.lstat(givenpath, getStats);
+  // TODO move givenpath to fileStats.xyz
+  create()('./testroot/absolute/real');
 }());
