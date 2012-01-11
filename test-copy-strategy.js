5c39b23c06ada8028fc179390b1dd7fed3d0fb11
AJ ONeal <coolaj86@gmail.com>
2012-01-11 10:05:16 -0500

    added comments

--- test-copy-strategy.js
+++ test-copy-strategy.js
@@ -1,14 +1,24 @@
 (function () {
   "use strict";
 
+  /*
+    Cases that need testing:
+    
+    a new file is added // most common
+    a symlink to a new file is added
+    a symlink to an existing file is added
+    an existing (md5 match) file is added
+  */
+
   var Copy = require('./copy-strategy')
     , copy = Copy.create()
     ;
 
-  function gotErDone(e) {
+  function gotErDone(e, stat) {
     console.error('[error]');
     console.error(e);
     console.log('All Done');
+    console.log(stat);
   }
 
   copy(gotErDone, './testroot/absolute/real');
