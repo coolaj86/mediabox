14012039c2f0711995430d87de8dcc10f359949c
AJ ONeal <coolaj86@gmail.com>
2012-01-13 21:20:27 -0700

    added file test case and some comments

--- create-testroot.sh
+++ create-testroot.sh
@@ -15,9 +15,19 @@ cd ./testroot/symbolic
   sleep 1
   ln -s ../absolute/real working.txt
   sleep 1
+  rsync ../absolute/real ../absolute/real.duplicate
+  sleep 1
   ln -s working.txt link-to-a-link
 cd -
 find testroot
 
 mkdir -p ./testdb
 mkdir -p ./testdb/tmp
+
+BLOCK_COMMENT="
+  expected result:
+    real -> md5
+    real.duplicate -> hardlinked to real
+    working.txt
+    link-to-a-link
+"
