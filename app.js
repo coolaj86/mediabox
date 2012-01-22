4a2db57ffc0ced136cf76a7cf5b24ab94e70ddc4
AJ ONeal <coolaj86@gmail.com>
2012-01-22 08:12:53 +0000

    added tag support to main app, needs regression testing

--- app.js
+++ app.js
@@ -8,9 +8,14 @@
   function importPath() {
     // accepts file or directory
     //path.normalize(process.argv[2] + '/')
+    /*
     mediabox.import(function () {
       console.log('done');
     }, scanpath);
+    */
+    console.log('big copy');
+    console.log(mediabox.caches.audio.all().length);
+    console.log('end big copy');
   }
 
   mediabox = MediaBox.create(config);
