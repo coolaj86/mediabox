ba4b25837c245663c5e40ad07547318f6b5ac900
AJ ONeal <coolaj86@gmail.com>
2012-01-22 23:32:27 +0000

    callback with error if no start path is given

--- app.js
+++ app.js
@@ -5,17 +5,22 @@
     , scanpath = process.argv[2]
     ;
 
-  function importPath() {
+  function importPath(err) {
     // accepts file or directory
     //path.normalize(process.argv[2] + '/')
-    /*
-    mediabox.import(function () {
+    ///*
+    mediabox.import(function (err) {
+      if (err) {
+        console.error(err.stack);
+      }
       console.log('done');
     }, scanpath);
-    */
+    //*/
+    /*
     console.log('big copy');
     console.log(mediabox.caches.audio.all().length);
     console.log('end big copy');
+    */
   }
 
   mediabox = MediaBox.create(config);
