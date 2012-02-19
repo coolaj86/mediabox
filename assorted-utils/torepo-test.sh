c6a409985f25a83bdf355a31f6ef42167761c233
AJ ONeal <coolaj86@gmail.com>
2012-02-18 21:15:32 -0700

    emptying master prior to major breaking change

--- assorted-utils/torepo-test.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p test_cur/test.d/test2.d/test3.d
-
-touch test_cur/test.a
-touch test_cur/test.d/test.b
-touch test_cur/test.d/test2.d/test.c
-touch test_cur/test.d/test2.d/test3.d/test.e
-
-mkdir test_repo
--- move-id3-tags.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# find ./db -name '[a-z0-9]*[a-z0-9][a-z0-9][a-z0-9][a-z0-9].json' -exec move-id3-tags.sh {} \;
-set -e 
-set -u
-
-JSON=${1}
-DIRNAME=`dirname ${1}`
-MD5SUM=`basename ${1} .json`
-mv ${JSON} ${DIRNAME}/${MD5SUM}.mp3.json
