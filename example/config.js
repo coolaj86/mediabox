47f4375d4805e4ca823840cc34cbdf644772f232
AJ ONeal <coolaj86@gmail.com>
2012-03-20 04:21:58 +0000

    ignore MediaBox, added comment about sync failing, updated config

--- example/config.js
+++ example/config.js
@@ -5,14 +5,15 @@
     ;
 
   config = {
-      dbroot: '../tests/testdb/'
+      dbroot: '/mnt/wd-large-1tb/MediaBox-storage/db/'
     // using the full dirname is too many chars for exec
     // dbroot: __dirname + '/' + '../tests/testdb/'
     //, tmproot: 'testdb/tmp'
     //, filesroot: 'testdb/files'
     //, metaroot: 'testdb/meta'
     //, audioroot: 'testdb/audio'
-    , automountDir: '../tests/automounts'
+    //, automountDir: '../tests/automounts'
+    , automountDir: '/mnt'
     , remove: false
     , hardlink: true
     , softlink: false
